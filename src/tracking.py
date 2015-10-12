Fixes bug with VideoStream.isFirstDataFrame (in GuiTracker and Tracker)     Removes method and does direct check Fixes tracking normalise option (tracking and video_frame)     Now uses average and mean of background as 8 bit scale Fixes typo and checks for float in video_analysis.plosToDistances Refactors video_frame (ndim instead of len(shape)

src/tracking.py
@@ -247,7 +247,7 @@ class Tracker(object):
                 elif self._stream.bgEndFrame < fid < self.trackFrom:
                     continue # Skip junk frames
                 else: # Tracked frame
-                    if self._stream.isFirstDataFrame(): self._finaliseBg()
+                    if fid == self.trackFrom: self._finaliseBg()
                     sil = self._trackFrame(frame)
                     if sil is None:
                         continue # Skip if no contour found
@@ -335,11 +335,11 @@ class Tracker(object):
         """
         Finalise the background (average stack and compute SD if more than one image)
         """
-        if self.normalise:
-            self.bg = self.bg.normalise()
         if self.bg.ndim > 2:
             self.bgStd = np.std(self.bg, axis=2)
             self.bg = np.average(self.bg, axis=2)
+        if self.normalise:
+            self.bgAvgAvg = self.bg.mean()# TODO: rename
     
     def _trackFrame(self, frame, requestedColor='r', requestedOutput='raw'):
         """
@@ -467,10 +467,11 @@ class Tracker(object):
         :rtype: video_frame.Frame
         """
         if self.normalise:
-            frame = frame.normalise()
+            frame = frame.normalise(self.bgAvgAvg)
         diff = Frame(cv2.absdiff(frame, self.bg))
         if self.bgStd is not None:
-            silhouette = diff > (self.bgStd * self.nSds)
+            threshold = self.bgStd * self.nSds
+            silhouette = diff > threshold
             silhouette = silhouette.astype(np.uint8) * 255
         else:
             diff = diff.astype(np.uint8)

src/video_analysis.py
@@ -60,7 +60,10 @@ def posToDistances(positions):
     for i in range(1, len(positions)):
         p1 = positions[i-1]
         p2 = positions[i]
-        distance = math.sqrt((p2[1] - p1[1])**2 * (p2[0] - p1[0])**2)
+        for v in p1+p2:
+            if type(v) not in [float, np.float64, np.float32]:
+                raise TypeError("Index {}, expected type float, got {}".format(i, type(v)))
+        distance = math.sqrt((p2[1] - p1[1])**2 + (p2[0] - p1[0])**2)
         distances.append(distance)
     return distances
 

src/video_frame.py
@@ -72,9 +72,9 @@ class Frame(np.ndarray):
         :return: the grayscale frame
         :rtype: `Frame`
         """
-        if len(self.shape) == 2: # Single channel images:
+        if self.ndim == 2: # Single channel images:
             return Frame(np.dstack([self]*3))
-        elif len(self.shape) == 3 and self.shape[2] == 1:
+        elif self.ndim == 3 and self.shape[2] == 1:
             raise NotImplementedError("Image is color but has only one channel.\
                                         This type is not supported yet")            
         return Frame(cv2.cvtColor(self, cv2.COLOR_BGR2GRAY))
@@ -86,7 +86,7 @@ class Frame(np.ndarray):
         :return: the color frame
         :rtype: `Frame`
         """
-        if len(self.shape) == 3 and self.shape[2] == 3: # Already color
+        if self.ndim == 3 and self.shape[2] == 3: # Already color
             return Frame(self.astype(np.uint8))
 #        return Frame(np.dstack([self.gray().astype(np.uint8)]*3))
         return Frame(self.gray())
@@ -107,19 +107,20 @@ class Frame(np.ndarray):
         code, silhouette = cv2.threshold(self, threshold, 255, cv2.THRESH_BINARY)
         return Frame(silhouette)
         
-    def normalise(self):
+    def normalise(self, refAvg=75):
         """
-        Normalises the frame (divides by median and keeps 8 bits)
+        Normalises the frame (divides by average and keeps 8 bits)
+        
+        :param int refAvg: The reference average to use to keep the 8 bits range
         
         :return: the normalised frame
         :rtype: `Frame`
         """
-#        avg = np.mean(self)
-        median = np.median(self)
+        avg = self.mean()
         originalDType = self.dtype
         frame = self.astype(np.float32)
-        frame /= median
-        frame *= 255 # TODO: use original range
+        frame /= avg
+        frame *= refAvg
         if originalDType != np.float32:
             frame = frame.astype(originalDType)
         return Frame(frame)