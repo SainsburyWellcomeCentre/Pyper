655c0895df0dc0e995c2a39d5d87130e9b9b38b0
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-23 14:07:24 +0000

    Adding tarfile member sanitization to extractall()

--- pyper/contours/roi.py
+++ pyper/contours/roi.py
@@ -70,7 +70,26 @@ class RoiCollection(object):
         tmp_archive_file_path = os.path.join(tmp_dir, archive_name)
         shutil.copy(archive_file_path, tmp_archive_file_path)
         with tarfile.open(tmp_archive_file_path) as tar:
-            tar.extractall(path=tmp_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path=tmp_dir)
         archive_dir = os.path.join(tmp_dir, '.')
         for file_name in os.listdir(archive_dir):
             if file_name.endswith('.roi'):
